5111db3384adb3036bd659291bd60d936e0944f1
Mathis <mathisfalik76@gmail.com>
2026-09-08 15:36:52 +0200

    test

--- TP1/prog1.py
+++ TP1/prog1.py
@@ -1,1 +1,9 @@
 print("Hello, World!")
+
+import fonctions as f
+
+while True:
+    a = int(input("Entrez le premier nombre (base) : "))
+    b = int(input("Entrez le second nombre (exposant) : "))
+    res = f.puissance(a, b)
+    print(a, "puissance", b, "=", res)
